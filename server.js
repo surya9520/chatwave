9f22e6a80428109641f3bc86044377866435524c
surya <suryajoshi617@gmail.com>
2024-09-02 12:38:21 +0530

    origin-updated

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 // Connect to the database
 connectDB();
 app.use(cors({
-origin: 'https://chatwave-ysq7.onrender.com/',
+origin: 'https://chatwave-ysq7.onrender.com',
 methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 credentials: true,
 })); 
