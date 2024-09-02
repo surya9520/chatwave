91828f3d30dab94717463bcb68d7e9b39d59eddc
surya <suryajoshi617@gmail.com>
2024-09-02 19:04:20 +0530

    cors updated

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');  
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const googleAuthRoutes = require('./routes/googleauth');
@@ -7,21 +8,27 @@ const googleAuthRoutes = require('./routes/googleauth');
 const app = express();
 
 // Connect to the database
-connectDB(); 
+connectDB();
+
+// Use CORS
+app.use(cors({
+    origin: 'https://chatopen-jaxj.vercel.app'  // Allow your frontend's origin
+}));
 
 app.use(express.json());
 
 app.get('/', (req, res) => { 
     res.json({ message: 'Hello, JSON' });
-  });
+});
+
 app.get('/surya', (req, res) => { 
     res.json({ message: 'Hello, surya' });
-  });
+});
 
 // Mount routes
 app.use('/auth', authRoutes);
 app.use('/auth/google', googleAuthRoutes);
 
 app.listen(5000, () => {
-console.log('Server running on port 5000');
+    console.log('Server running on port 5000');
 });
