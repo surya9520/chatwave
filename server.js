6c29032c3aa7df5ef56670b51ff13b305344813c
surya <suryajoshi617@gmail.com>
2024-09-02 12:12:27 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const googleAuthRoutes = require('./routes/googleauth');
+
+const app = express();
+
+// Connect to the database
+connectDB();
+app.use(cors({
+origin: 'http://localhost:3000',
+methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+credentials: true,
+})); 
+
+app.use(express.json());
+
+app.get('/', (req, res) => { 
+    res.json({ message: 'Hello, JSON' });
+  });
+
+// Mount routes
+app.use('/api/auth', authRoutes);
+app.use('/api/auth/google', googleAuthRoutes);
+
+app.listen(5000, () => {
+console.log('Server running on port 5000');
+});
